d48ee78b97f9fc952b0edebe08c687111d8757a3
RedChestRaven <redchestraven@gmail.com>
2022-11-20 14:23:47 +0100

    Moved RateOfDecay to be DecayingFoodGroup-dependent

--- src/main/java/com/redchestraven/food/fooddecay/customtypes/DecayingFoodGroup.java
+++ src/main/java/com/redchestraven/food/fooddecay/customtypes/DecayingFoodGroup.java
@@ -7,17 +7,24 @@ import java.util.ArrayList;
 public class DecayingFoodGroup
 {
 	private String _name;
+	private int _rateOfDecay;
 	private ArrayList<Material> _decayingFoods;
 
-	public DecayingFoodGroup(String name, ArrayList<Material> decayingFoods)
+	public DecayingFoodGroup(String name, int rateOfDecay, ArrayList<Material> decayingFoods)
 	{
 		_name = name;
+		_rateOfDecay = rateOfDecay;
 		_decayingFoods = decayingFoods;
 	}
 
 	public String GetName()
 	{
 		return _name;
+	}
+
+	public int GetRateOfDecay()
+	{
+		return _rateOfDecay;
 	}
 
 	public ArrayList<Material> GetDecayingFoods()
